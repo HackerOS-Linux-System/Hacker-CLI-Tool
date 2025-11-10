f0d51bd3fc201939e4c7f9130ec4af45a239f342
michal92299 <chudybyczek2137@gmail.com>
2025-11-10 10:48:04 +0100

    Delete hacker/atomic directory

--- hacker/atomic/src/help.rs
+++
@@ -1,8 +0,0 @@
-use colored::*;
-use std::fs;
-pub fn display_ascii() {
-    match fs::read_to_string("/usr/share/HackerOS/Config-Files/HackerOS-Ascii") {
-        Ok(content) => println!("{}", content.bright_cyan().bold().on_black()),
-        Err(_) => println!("{}", "File not found".red().bold().on_black()),
-    }
-}
