40845ccd163609286cfe73f88c8bd9adfd7878a6
michal92299 <chudybyczek2137@gmail.com>
2026-01-08 23:17:36 +0100

    Update unpack_commands.cr

--- source-code/hacker/src/unpack_commands.cr
+++ source-code/hacker/src/unpack_commands.cr
@@ -1,4 +1,6 @@
+# unpack_commands.cr
 require "./helpers"
+
 def handle_unpack(args : Array(String))
   if args.empty? || args[0] == "help"
     show_unpack_help
@@ -20,7 +22,7 @@ def handle_unpack(args : Array(String))
     safe_run("sudo apt install -y crystal && sudo apt install -y shards")
     safe_run("sudo apt install -y npm && sudo apt install -y nodejs")
     safe_run("flatpak install -y flathub com.visualstudio.code")
-    safe_run("curl --proto '=https' --tlsv1.2 -sSf <https://sh.rustup.rs> | sh")
+    safe_run("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh")
     safe_run("sudo apt install -y golang-go")
     safe_run("sudo apt install -y lua5.4")
     safe_run("sudo snap install zig --beta --classic")
@@ -46,12 +48,12 @@ def handle_unpack(args : Array(String))
       safe_run("flatpak install -y flathub org.vinegarhq.Vinegar")
     end
   when "hacker-mode"
-    safe_run("git clone <https://github.com/HackerOS-Linux-System/Hacker-Mode.git> /tmp/Hacker-Mode")
+    safe_run("git clone https://github.com/HackerOS-Linux-System/Hacker-Mode.git /tmp/Hacker-Mode")
     safe_run("hl run /tmp/Hacker-Mode/unpack.hacker")
   when "gamescope-session-steam"
     safe_run("flatpak install -y flathub com.valvesoftware.Steam")
     install_gamescope
-    safe_run("git clone <https://github.com/HackerOS-Linux-System/gamescope-session-steam.git> /tmp/gamescope-session-steam")
+    safe_run("git clone https://github.com/HackerOS-Linux-System/gamescope-session-steam.git /tmp/gamescope-session-steam")
     safe_run("hl run /tmp/gamescope-session-steam/unpack.hacker")
   when "xanmod"
     safe_run("/usr/share/HackerOS/Scripts/Bin/unpack-xanmod.sh")
@@ -66,13 +68,13 @@ def handle_unpack(args : Array(String))
     safe_run("cp /usr/share/HackerOS/Archived/alacritty.toml ~/.config/alacritty/alacritty.toml")
     puts "#{Colors::GREEN}Alacritty configuration has been successfully installed to ~/.config/alacritty/alacritty.toml#{Colors::RESET}"
   when "hackeros-tv"
-    safe_run("git clone <https://github.com/HackerOS-Linux-System/HackerOS-TV.git> /tmp/HackerOS-TV")
+    safe_run("git clone https://github.com/HackerOS-Linux-System/HackerOS-TV.git /tmp/HackerOS-TV")
     safe_run("hl run /tmp/HackerOS-TV/unpack.hacker")
   when "security-mode"
-    safe_run("git clone <https://github.com/HackerOS-Linux-System/Security-Mode.git> /tmp/Security-Mode")
+    safe_run("git clone https://github.com/HackerOS-Linux-System/Security-Mode.git /tmp/Security-Mode")
     safe_run("hl run /tmp/Security-Mode/unpack.hacker")
   when "winboat"
-    safe_run("wget <https://github.com/TibixDev/winboat/releases/download/v0.9.0/winboat-0.9.0-amd64.deb> -O /tmp/winboat-0.9.0-amd64.deb")
+    safe_run("wget https://github.com/TibixDev/winboat/releases/download/v0.9.0/winboat-0.9.0-amd64.deb -O /tmp/winboat-0.9.0-amd64.deb")
     safe_run("sudo apt install -y /tmp/winboat-0.9.0-amd64.deb")
   when "nvidia-drivers"
     safe_run("/usr/share/HackerOS/Scripts/Bin/unpack-nvidia-drivers.sh")
@@ -89,10 +91,9 @@ def handle_unpack(args : Array(String))
     safe_run("tar -xzf /tmp/HackerScript.tar.gz -C /tmp")
     # Assuming the tar.gz extracts to a directory or directly files; adjust if needed
     # For virus and hsc to /usr/bin
+    safe_run("sudo mv /tmp/HS1 /home/HackerScript/bin/")
     safe_run("sudo mv /tmp/virus /usr/bin/virus") # Assume files are extracted to /tmp
     safe_run("sudo chmod a+x /usr/bin/virus")
-    safe_run("sudo mv /tmp/hsc /usr/bin/hsc")
-    safe_run("sudo chmod a+x /usr/bin/hsc")
     # For hs1 hs2 hs3 to /home/HackerScript/bin/
     safe_run("mv /tmp/hs1 /home/HackerScript/bin/hs1")
     safe_run("chmod a+x /home/HackerScript/bin/hs1")
@@ -103,12 +104,16 @@ def handle_unpack(args : Array(String))
   when "flox"
     safe_run("wget https://downloads.flox.dev/by-env/stable/deb/flox.x86_64-linux.deb -O /tmp/flox.x86_64-linux.deb")
     safe_run("sudo apt install -y /tmp/flox.x86_64-linux.deb")
+  when "hackeros-builder"
+    safe_run("wget https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Builder/main/install.hacker -O /tmp/install.hacker")
+    safe_run("hl run /tmp/install.hacker")
   else
     puts "#{Colors::RED}Unknown unpack subcommand: #{subcommand}#{Colors::RESET}"
     show_unpack_help
     exit(1)
   end
 end
+
 def show_unpack_help
   puts "#{Colors::BOLD}#{Colors::MAGENTA}Unpack subcommands:#{Colors::RESET}"
   puts " #{Colors::GRAY}add-ons #{Colors::RESET}- Install wine and related tools"
@@ -132,4 +137,5 @@ def show_unpack_help
   puts " #{Colors::GRAY}hl-utils #{Colors::RESET}- Install hl-utils binaries"
   puts " #{Colors::GRAY}hackerscript #{Colors::RESET}- Install HackerScript"
   puts " #{Colors::GRAY}flox #{Colors::RESET}- Install Flox"
+  puts " #{Colors::GRAY}hackeros-builder #{Colors::RESET}- Install HackerOS Builder"
 end
