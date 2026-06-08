3e03b1e278d30753acf936d149ee6e929f86d951
michal92299 <chudybyczek2137@gmail.com>
2026-06-08 08:33:50 +0200

    Create constants.cr

---
+++ source-code/update-system/constants.cr
@@ -0,0 +1,6 @@
+# ── Paths ────────────────────────────────────────────────────────────────────
+HACKEROS_UPDATE_SCRIPT   = "/usr/bin/hl run /usr/share/HackerOS/Scripts/Bin/update-hackeros.hl"
+WALLPAPERS_UPDATE_SCRIPT = "/usr/bin/hl run /usr/share/HackerOS/Scripts/Bin/update-wallpapers.hl"
+BIN_PATH                 = Process.executable_path.not_nil!
+
+DISTRO_RC_PATH = "/etc/xdg/kcm-about-distrorc"
